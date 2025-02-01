c81b6f109496bf9dd27460935095005ba8527e64
yayoimizuha <poko.peko.tomo@icloud.com>
2025-02-02 04:27:57 +0900

    update

--- main.py
+++ main.py
@@ -238,6 +238,7 @@ def post_reply(destination: int, resp_text: Optional[str] = None, image_path: Op
                 multipart=mp
             )
             print(resp.text)
+            print(mp)
             media_attachments.append(resp.json()["id"])
     # noinspection PyProtectedMember
     api._make_session().post(
